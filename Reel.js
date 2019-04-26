28112098e17c38c4b8d05cc67fa9e9386434a697
Joshua P. Hendrix <joshuaphendrix@gmail.com>
2019-04-26 12:22:05 -0400

    small code cleanup and adding cypher visual

--- Reel.js
+++ Reel.js
@@ -1,13 +1,15 @@
 
 
 class Reel {
-  constructor(plain_text,cypher_text) {
+  constructor(plain_text,cypher_text,key) {
     //TODO: error checking.  text arrays need to be same size and contain unique letters.
     //Convert inputs to an array so that they are easier to shift
     this.plain_text = plain_text.split('');
     this.cypher_text = cypher_text.split('');
     this.shift_position = 0;
     this.wheel_size = plain_text.length;
+		this.key = key;
+		this.setKey(key);
   }
 
   get state() {
@@ -36,11 +38,12 @@ class Reel {
     return;
   }
 
-  encode(plain_text,key) {
+  encode(plain_text) {
     let chars = plain_text.split(''); //convert to array
     let cypher_text = ''; //return value
 
-    this.setKey(key);
+		console.log(this.key);
+    this.setKey(this.key);
 
     //For each of the characters of the input
     for(let i=0; i<chars.length; i++){
@@ -61,13 +64,13 @@ class Reel {
     return cypher_text;
   }
 
-  decode(cypher_text,key) {
+  decode(cypher_text) {
     //convert to an array and reverse
     //start decrypting from the end and shift in reverse
     let chars = cypher_text.split('').reverse();
     let plain_text = ''; //return value
 
-    this.setKey(key+chars.length);
+    this.setKey(this.key+chars.length);
 
     //For each of the characters of the input
     for(let i=0; i<chars.length; i++){
